b88e78f30a95f4f8fcb187cf3b4116350acf04ff
yaozeyuan001 <yaozeyan001@ke.com>
2021-12-25 21:47:20 +0800

    feat: 更换缓存文件夹

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://cdn.jsdelivr.net/gh/YaoZeyuan/loan_calculator@master/dist/",
+  base: "https://cdn.jsdelivr.net/gh/YaoZeyuan/loan_calculator@master/docs/",
   plugins: [react()],
   build: {
     "outDir": "docs",
