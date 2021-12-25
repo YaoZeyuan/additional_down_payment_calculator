e4c4d41f582c3d3ec9035f967b965258a7a06a58
yaozeyuan001 <yaozeyan001@ke.com>
2021-12-25 21:43:28 +0800

    feat: 测试github page功能

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://cdn.jsdelivr.net/gh/YaoZeyuan/loan_calculator@master/dist/",
   plugins: [react()],
   resolve: {
     alias: [
